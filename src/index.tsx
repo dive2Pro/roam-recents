efd08c5d8446974d0bbbc7b5574db7c846dc1c67
黄艺川 <toylucklebo@gmail.com>
2025-02-07 20:58:02 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,171 @@
-import { extension_helper } from "./helper";
-import { initExtension } from "./extension";
-import { initTopbarIcon } from "./topbar-icon";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, ButtonGroup } from "@blueprintjs/core";
 
-function onload({ extensionAPI }: { extensionAPI: RoamExtensionAPI }) {
-  initTopbarIcon(extensionAPI);
-  initExtension();
+declare global {
+  interface Window {
+    roamAlphaAPI: any;
+  }
 }
 
-function onunload() {
-  extension_helper.uninstall();
+const getCurrentPageInfo = async () => {
+  let current_uid =
+    await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
+  if (!current_uid) {
+    throw new Error("not in page");
+  }
+  let pageInfo = window.roamAlphaAPI.q(`
+        [
+        :find  (pull ?c [ :block/uid :node/title ]) .
+        :where
+        [?b :block/uid "${current_uid}"]
+        [?b :block/page ?c]
+       
+        ]
+        `);
+  if (pageInfo) {
+    return pageInfo;
+  }
+  return window.roamAlphaAPI.q(`
+        [
+        :find  (pull ?c [ :block/uid :node/title ]) .
+        :where
+        [?c :block/uid "${current_uid}"]
+       
+       
+        ]
+        `);
+};
+const CACHE_KEY = "__roam_recents";
+class Cache {
+  sync(json: string) {
+    localStorage.setItem(CACHE_KEY, json);
+  }
+  read(): { uid: string; title: string }[] {
+    try {
+      return JSON.parse(localStorage.getItem(CACHE_KEY)) || [];
+    } catch (e) {
+      return [];
+    }
+  }
 }
+const cache = new Cache();
+
+function History(props: { hide?: boolean }) {
+  const [recents, setRecents] = React.useState(() => cache.read());
+
+  React.useEffect(() => {
+    async function onHashChange() {
+      const pageInfo = await getCurrentPageInfo();
+      const found_index = recents.findIndex((item) => {
+        return item.uid === pageInfo.uid;
+      });
+      if (found_index === -1) {
+      } else {
+        recents.splice(found_index, 1);
+      }
+      recents.unshift(pageInfo);
+      cache.sync(JSON.stringify(recents));
+      setRecents([...recents]);
+    }
+    window.addEventListener("hashchange", onHashChange);
+    return () => {
+      window.removeEventListener("hashchange", onHashChange);
+    };
+  }, []);
+
+  if (props.hide) {
+    return null;
+  }
+  return (
+    <section
+      style={{
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        backgroundColor: "#182026",
+      }}
+    >
+      {recents.map((recent) => {
+        return (
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              if (e.shiftKey) {
+                window.roamAlphaAPI.ui.rightSidebar.addWindow({
+                  window: { type: "block", "block-uid": recent.uid },
+                });
+                return false;
+              }
+              window.roamAlphaAPI.ui.mainWindow.openPage({
+                page: { uid: recent.uid },
+              });
+              return false;
+            }}
+          >
+            <div className="page">{recent.title}</div>
+          </a>
+        );
+      })}
+    </section>
+  );
+}
+
+function NavMenu() {
+  const [active, setActive] = React.useState("star");
+  const isActive = (_active: string) => {
+    return active === _active;
+  };
+  console.log(" nav render ")
+  return (
+    <ButtonGroup fill className="bp3-dark">
+      <Button
+        intent={isActive("star") ? "primary" : "none"}
+        onClick={() => {
+          setActive("star");
+          ReactDOM.render(<History hide={true} />, div);
+        }}
+        icon="star"
+        active={isActive("star")}
+      >
+        <small>Shorcuts</small>
+      </Button>
+
+      <Button
+        intent={isActive("time") ? "primary" : "none"}
+        onClick={() => {
+          setActive("time");
+          ReactDOM.render(<History />, div);
+        }}
+        icon="time"
+        active={isActive("time")}
+      >
+        <small>Recents</small>
+      </Button>
+    </ButtonGroup>
+  );
+}
+
+let unload = () => {
+  //
+};
+const div = document.createElement("div");
 
 export default {
-  onload,
-  onunload,
+  onload() {
+    const el_starred_pages = document.querySelector(".starred-pages");
+    const el = el_starred_pages.previousElementSibling;
+    el_starred_pages.appendChild(div);
+    console.log(" loaded ");
+    ReactDOM.render(<NavMenu />, el);
+    unload = () => {
+      div.remove();
+    };
+  },
+  onunload() {
+    unload();
+  },
 };
